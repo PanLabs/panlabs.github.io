5ac91651eede06746f5643828d7df0c51fb9ef4c
Corey Howell <cjthowell@gmail.com>
2017-03-27 19:55:43 -0400

    small fix to link for hubspot

--- vendor/hubspot/exit-modal.js
+++ vendor/hubspot/exit-modal.js
@@ -18,7 +18,7 @@
   for (var name in attrs) { if(attrs.hasOwnProperty(name)) { js.setAttribute(name, attrs[name]); } }
   var e = document.getElementsByTagName('script')[0];
   e.parentNode.insertBefore(js, e);
-})('LeadFlows-2973568', 'leadflows.js', {"data-leadin-portal-id":2973568,"data-leadin-env":"prod","data-loader":"hs-scriptloader"});
+})('LeadFlows-2973568', 'vendor/hubspot/leadflows.js', {"data-leadin-portal-id":2973568,"data-leadin-env":"prod","data-loader":"hs-scriptloader"});
 
 (function (id, src, attrs) {
   if (document.getElementById(id)) { return; }
